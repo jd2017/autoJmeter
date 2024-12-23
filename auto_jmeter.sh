9f5faf21d6ed35822194c55b0da5e02546485b43
u012874998 <964321735@qq.com>
2024-12-23 16:46:52 +0800

    first commit

---
+++ auto_jmeter.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# 压测脚本中设定的压测时间应为20秒
+export jmx_filename="baidu_request.jmx"
+
+# 需要在系统变量中定义jmeter根目录的位置，如下
+export jmeter_path="/Users/jd/soft/apache-jmeter-5.6.3"
+
+echo "自动化压测开始"
+
+# 压测并发数列表
+thread_number_array=(1 2 3)
+for num in "${thread_number_array[@]}"
+do
+    echo "压测并发数 ${num}"
+    # 定义jtl结果文件名与压测报告路径
+    export jtl_filename="test_${num}.jtl"
+    export web_report_path_name="web_${num}"
+
+    rm -f ${jtl_filename}
+    rm -rf ${web_report_path_name}
+
+    # JMeter 静默压测 + 生成html压测报告
+    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename}  -Jthread=${num} -e -o ${web_report_path_name}
+    echo "结束压测并发数 ${num}"
+done
+echo "自动化压测全部结束"
